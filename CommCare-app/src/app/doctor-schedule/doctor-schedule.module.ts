fe7bbbd0fe7364f40dbb7436e1d7717da30003bb
OmniaMoustafa <omnia_moustafa@outlook.com>
2018-05-20 13:10:15 +0200

    fff

---
+++ CommCare-app/src/app/doctor-schedule/doctor-schedule.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DoctorScheduleComponent } from './doctor-schedule.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DoctorScheduleComponent]
+})
+export class DoctorScheduleModule { }
