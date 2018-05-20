02aec4c1aa0d727e0182ea7738a587ee8b9048a7
Omnia-Ameer <omnia.ameer20@gmail.com>
2018-05-20 23:58:55 +0200

    Om-Am

--- CommCare-app/src/app/app.module.ts
+++ CommCare-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventAddComponent } from './events/event-add/event-add.component';
+import { EventListingComponent } from './events/event-listing/event-listing.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventAddComponent,
+    EventListingComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule
