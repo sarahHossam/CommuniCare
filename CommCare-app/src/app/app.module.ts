c8b9b8c7bb24a20b00534e6b8f7cc7b0967b022c
sarahHossam <sarahossam50@gmail.com>
2018-05-22 09:47:55 -0700

    resolve conflicts

--- CommCare-app/src/app/app.module.ts
+++ CommCare-app/src/app/app.module.ts
@@ -2,24 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { DoctorItemSmallComponent } from 'src/app/doctors/doctor-item-small/doctor-item-small.component';
-=======
 import { EventAddComponent } from './events/event-add/event-add.component';
 import { EventListingComponent } from './events/event-listing/event-listing.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
->>>>>>> 95642536bdfde69d2247dd631f1be6bd7edba142
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     DoctorItemSmallComponent,
-=======
     EventAddComponent,
     EventListingComponent,
     EventDetailsComponent
->>>>>>> 95642536bdfde69d2247dd631f1be6bd7edba142
   ],
   imports: [
     BrowserModule
