9857fc93afdbe16da4558dba55897071cb34d5df
sarahHossam <sarahossam50@gmail.com>
2018-05-21 20:46:44 -0700

    vvvv

--- CommCare-app/src/app/app.module.ts
+++ CommCare-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DoctorItemSmallComponent } from 'src/app/doctors/doctor-item-small/doctor-item-small.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorItemSmallComponent,
   ],
   imports: [
     BrowserModule
