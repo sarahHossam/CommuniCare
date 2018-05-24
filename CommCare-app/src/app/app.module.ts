70ebd64d23455d547a7f25a49ff3e1001c3083de
Emanawwad <emanabdelawwad@gmail.com>
2018-05-24 10:02:09 +0200

    eman module

--- CommCare-app/src/app/app.module.ts
+++ CommCare-app/src/app/app.module.ts
@@ -2,11 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HospitalListingComponent } from './hospitals/hospital-listing/hospital-listing.component';
+import {HospitalitemComponent} from './hospitals/hospital-listing/hospitalitem/hospitalitem.component';
+import {HospitalsComponent} from './hospitals/hospitals.component';
+import {HospitalDetailsComponent} from './hospitals/hospital-details/hospital-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HospitalListingComponent,
+    HospitalitemComponent,
+    HospitalsComponent,
+    HospitalDetailsComponent 
+   ],
   imports: [
     BrowserModule
   ],
